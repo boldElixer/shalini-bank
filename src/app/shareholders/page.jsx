6db7c714b21b4cfe9dbe4009460e5abf6e423932
Ashwin <ashwinsrk77@gmail.com>
2025-12-15 15:06:07 +0530

    shareholder info updated

--- src/app/shareholders/page.jsx
+++ src/app/shareholders/page.jsx
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
 import styles from './shareholders.module.css';
@@ -19,7 +18,13 @@ export default function ShareholdersPage() {
             <section className={styles.section}>
                 <h1>Shareholders</h1>
                 <p>
-                    At Shalini Sahakari Bank, our shareholders are the cornerstone of our cooperative model, embodying the spirit of community ownership and participation. We are committed to maintaining transparent communication and fostering a collaborative relationship with our shareholders. Through regular updates, annual general meetings, and inclusive decision-making processes, we ensure that our shareholders are well-informed and actively engaged in the bank's growth and governance. Together, we strive to build a resilient financial institution that serves the needs of our members and the broader community.
+                    Shalini Sahakari Bank(SSB) shares are held in physical form. The face value and market value of each share is <b>₹10</b>. The minimum number of shares to be held by any member is <b>10 shares</b>.
+                </p>
+                <p>
+                    Please note that if the dividend remains unencashed on or before 31st December, 2024, it shall stand forfeited by the Bank and the proceeds thereof will be transferred to the Bank's Reserve Fund, in accordance with the provisions of the Bank.
+                </p>
+                <p>
+                    This intimation may be treated as the final notice to all concerned members who are yet to encash their dividends.
                 </p>
             </section>
         </div>
