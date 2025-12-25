e01fee2f0ac6649e7435d1c2360bf9adf263e790
Ashwin <ashwinsrk77@gmail.com>
2025-12-25 16:26:42 +0530

    awareness pdf added

--- src/app/awareness-campaign/page.jsx
+++ src/app/awareness-campaign/page.jsx
@@ -22,16 +22,20 @@ export default function AwarenessCampaignPage() {
                 <p>
                     While the digital technology has made our lives easier and more convenient, it has also made it easier for the fraudsters to trick and cheat us. Learn more about such fraud, and how you can stay safe by being vigilant.
                 </p>
-                <br/>
-                <p><Link href="https://rbidocs.rbi.org.in/rdocs/content/pdfs/BEAWARE07032022.pdf" target="_blank" rel="noopener noreferrer" className={styles.linkButton}>
-                    BE(A)WARE Booklet- A Booklet on Modus Operandi of Financial Frauds
-                </Link></p>
-                <p><Link href="https://www.rbi.org.in/commonman/english/Scripts/BasicBankingNew.aspx" target="_blank" rel="noopener noreferrer" className={styles.linkButton}>
-                    Raju & The Forty Thieves
-                </Link></p>
-                <p><Link href="https://www.rbi.org.in/financialeducation/RajuandATM.aspx" target="_blank" rel="noopener noreferrer" className={styles.linkButton}>
-                    Raju & The Friendly ATM
-                </Link></p>
+                <section className={styles.notice}>
+                    <Link href="/awareness-campaign.pdf" target="_blank" rel="noopener noreferrer" className={styles.linkButton}>
+                        RBI Nationwide Intensive Financial Awareness Campaign
+                    </Link>
+                    <Link href="https://rbidocs.rbi.org.in/rdocs/content/pdfs/BEAWARE07032022.pdf" target="_blank" rel="noopener noreferrer" className={styles.linkButton}>
+                        BE(A)WARE Booklet on Modus Operandi of Financial Frauds
+                    </Link>
+                    <Link href="https://www.rbi.org.in/commonman/english/Scripts/BasicBankingNew.aspx" target="_blank" rel="noopener noreferrer" className={styles.linkButton}>
+                        Raju & The Forty Thieves
+                    </Link>
+                    <Link href="https://www.rbi.org.in/financialeducation/RajuandATM.aspx" target="_blank" rel="noopener noreferrer" className={styles.linkButton}>
+                        Raju & The Friendly ATM
+                    </Link>
+                </section>
                 <section className={styles.do_donts}>
                     <h2 style={{color: '#28B71A'}}>Safe Banking Practices - What to Do</h2>
                     <ul>
