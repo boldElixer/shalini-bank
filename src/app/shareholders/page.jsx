277d2f4fca959610f99790cddfb7899d2ecc1ac3
Ashwin <ashwinsrk77@gmail.com>
2026-01-01 21:40:38 +0530

    minor changes

--- src/app/shareholders/page.jsx
+++ src/app/shareholders/page.jsx
@@ -21,12 +21,6 @@ export default function ShareholdersPage() {
                 <p>
                     Shalini Sahakari Bank(SSB) shares are held in physical form. The face value and market value of each share is <b>₹100</b>. The minimum number of shares to be held by any member is <b>10 shares</b>.
                 </p>
-                <p>
-                    Please note that if the dividend remains unencashed on or before 31st December, 2024, it shall stand forfeited by the Bank and the proceeds thereof will be transferred to the Bank's Reserve Fund, in accordance with the provisions of the Bank.
-                </p>
-                <p>
-                    This intimation may be treated as the final notice to all concerned members who are yet to encash their dividends.
-                </p>
                 <p>Legal heirs may raise death claim of their deceased member/s.</p>
                 <br/>
                 <Link href="/forms/Membership_Form.pdf" target="_blank" rel="noopener noreferrer" className={styles.DownloadBtn}>Click Here to download Membership Form</Link>
